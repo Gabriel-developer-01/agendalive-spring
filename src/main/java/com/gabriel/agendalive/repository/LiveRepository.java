654295f92d411c608ee920eda3642f33a4bd01c3
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-07-23 00:01:59 -0300

    inserindo sort liveDate

--- src/main/java/com/gabriel/agendalive/repository/LiveRepository.java
+++ src/main/java/com/gabriel/agendalive/repository/LiveRepository.java
@@ -10,6 +10,6 @@ import com.gabriel.agendalive.document.LiveDocument;
 
 public interface LiveRepository extends MongoRepository<LiveDocument, String> {
 
-	 Page<LiveDocument> findByLiveDateAfter(LocalDateTime date, Pageable pageable);
-	 Page<LiveDocument> findByLiveDateBefore(LocalDateTime date, Pageable pageable);
+	Page<LiveDocument> findByLiveDateAfterOrderByLiveDateAsc(LocalDateTime date, Pageable pageable);
+    Page<LiveDocument> findByLiveDateBeforeOrderByLiveDateDesc(LocalDateTime date, Pageable pageable);
 } 
